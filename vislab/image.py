f98bc60616c92b526047234588e67f018c53af61
dreamingDream <vanishing.stay@gmail.com>
2014-06-29 15:54:20 +1000

    'hello'

--- vislab/image.py
+++
@@ -1,9 +0,0 @@
-import skimage
-
-
-def get_image_for_filename(image_filename):
-    if image_filename is not None:
-        image = skimage.io.imread(image_filename)
-        return image
-    else:
-        return None
